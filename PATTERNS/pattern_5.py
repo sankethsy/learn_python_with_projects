8977eae84f9496809cc57545a40d5ec8af556c41
Sanketh <sankethsanketh89@gmail.com>
2026-07-16 18:17:17 +0530

    created pattern folder

---
+++ PATTERNS/pattern_5.py
@@ -0,0 +1,9 @@
+rows=int(input("enter : "))
+symbol=input("enter symbol : ")
+
+for x in range(1,rows+1) :
+    for y in range(rows-x) :
+        print(" ",end="")
+    for z in range(2*x-1) :
+        print(symbol,end="")
+    print()
