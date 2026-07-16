c965a41405f6574b0d2d733fd2265e0f84b21e2c
Sanketh <sankethsanketh89@gmail.com>
2026-07-16 19:52:50 +0530

    practiced list

---
+++ PATTERNS/Readme.md
@@ -0,0 +1,1 @@
+# In This Github Repo I will share My python Journey Along with Projects
---
+++ shopping_cart.py
@@ -0,0 +1,30 @@
+# I am using list to store items in the shopping cart
+Food=[]
+Prices=[]
+Total=0.0
+while True :
+    food=input("Enter the food to buy and 'q' to quit: ")
+    if food=="q" or food =="Q" :
+        break
+    else:
+        price=float(input(f"Enter the Price of a {food}: $ "))
+        Food.append(food)
+        Prices.append(price)
+
+print("------------YOUR ITEMS ARE------------")
+
+for food in Food :
+    print(food,end=" ")
+    
+
+for Price in Prices :
+    Total+=Price
+
+print()
+
+
+print(f"YOUR TOTAL ITEMS WORTH : ${Total:.2f}")
+
+print("----------THANKYOU FOR VISITING------------")
+
+
