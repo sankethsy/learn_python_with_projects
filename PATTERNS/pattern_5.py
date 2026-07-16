fc6dea8b3052ae75c98f3384857c4c3a46e3d85c
Sanketh <sankethsanketh89@gmail.com>
2026-07-16 18:20:49 +0530

    diamond shape pattern

--- PATTERNS/pattern_5.py
+++ PATTERNS/pattern_5.py
@@ -1,3 +1,13 @@
+#   *
+#   ***
+#  *****
+# *******
+# *******
+#  *****
+#   ***
+#    *
+
+# -------------
 rows=int(input("enter : "))
 symbol=input("enter symbol : ")
 
@@ -7,3 +17,11 @@ for x in range(1,rows+1) :
     for z in range(2*x-1) :
         print(symbol,end="")
     print()
+
+for x in range(rows,0,-1) :
+    for y in range(rows-x) :
+        print(" ",end="")
+    for z in range(2*x-1) :
+        print(symbol,end="")
+    print()
+    
