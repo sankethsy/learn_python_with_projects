9a95d3d4cc0adab5efa5ee3afd26e33a14e18942
Sanketh <sankethsanketh89@gmail.com>
2026-07-18 18:47:43 +0530

    renamed

--- PATTERNS/pattern_5.py
+++ PATTERNS/pattern_5.py
@@ -1,27 +1,46 @@
-#   *
-#   ***
-#  *****
-# *******
-# *******
-#  *****
-#   ***
-#    *
+# Rows: 5
+# Symbol: #
+#     #
+#   #####
+#  #######
+# #########
+rows = int(input("Rows: "))
+symbol = input("Symbol: ")
 
-# -------------
-rows=int(input("enter : "))
-symbol=input("enter symbol : ")
+for i in range(1, rows + 1):
 
-for x in range(1,rows+1) :
-    for y in range(rows-x) :
-        print(" ",end="")
-    for z in range(2*x-1) :
-        print(symbol,end="")
+    # Print spaces
+    for j in range(rows-i):
+        print(" ", end="")
+
+    # Print stars
+    for k in range(2*i-1):
+        print(symbol, end="")
+
     print()
 
-for x in range(rows,0,-1) :
-    for y in range(rows-x) :
-        print(" ",end="")
-    for z in range(2*x-1) :
-        print(symbol,end="")
+    #Inverted pyramid
+
+# Rows: 5
+# Symbol: #
+# #########
+#  #######
+#   #####
+#    ###
+#     #
+
+rows = int(input("Rows: "))
+symbol = input("Symbol: ")
+
+for i in range(rows,0,-1):
+
+    # Print spaces
+    for j in range(rows-i):
+        print(" ", end="")
+
+    # Print stars
+    for k in range(2*i-1):
+        print(symbol, end="")
+
     print()
-    
+
